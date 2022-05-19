0a1fbc02403aafde416f668f11f0b50bf5961161
dev81log <dev81log@gmail.com>
2022-05-19 12:09:47 -0300

    Finalizado programa para Editar Texto

--- Program.cs
+++ Program.cs
@@ -23,15 +23,58 @@ namespace EditorTexto
             {
                 case 0: Environment.Exit(0); break;
                 case 1: AbrirArquivo(); break;
-                case 2: CriarNovoArquivo(); break;
+                case 2: EditarArquivo(); break;
                 default: Menu(); break;
             }
-
         }
 
-        static void AbrirArquivo() { }
+        static void AbrirArquivo()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o caminho do arquivo para abrir:");
+            string caminho = Console.ReadLine();
 
-        static void CriarNovoArquivo() { }
+            using (var arquivo = new StreamReader(caminho))
+            {
+                string texto = arquivo.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+            Console.WriteLine("");
+            Thread.Sleep(10000);
+            Menu();
+        }
+
+        static void EditarArquivo()
+        {
+            Console.WriteLine("Digite seu texto de arquivo (ESC sair)");
+            Console.WriteLine("---------------------------");
+            string texto = "";
+
+            do
+            {
+                texto += Console.ReadLine();
+                texto += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            CriarArquivo(texto);
+        }
+
+        static void CriarArquivo(string texto)
+        {
+            Console.Clear();
+            Console.WriteLine("Passe o caminho onde o arquivo sera salvo");
+            var caminho = Console.ReadLine();
+
+            using (var arquivo = new StreamWriter(caminho))
+            {
+                arquivo.Write(texto);
+            }
+
+            Console.WriteLine($"Salvo arquivo {caminho} com sucesso");
+            Thread.Sleep(2000);
+            Menu();
+        }
     }
 }
 
