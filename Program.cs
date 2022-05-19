1103fe50df75e84f63aab5dbc3504a557b33b461
dev81log <dev81log@gmail.com>
2022-05-18 22:10:22 -0300

    Iniciando aplicação base Editor de Texto

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EditorTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha as opções: ");
+            Console.WriteLine("1 - Abrir o Arquivo");
+            Console.WriteLine("2 - Criar novo Arquivo");
+            Console.WriteLine("0 - Sair do programa");
+
+            short opcoesMenu = short.Parse(Console.ReadLine());
+
+            switch (opcoesMenu)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: AbrirArquivo(); break;
+                case 2: CriarNovoArquivo(); break;
+                default: Menu(); break;
+            }
+
+        }
+
+        static void AbrirArquivo() { }
+
+        static void CriarNovoArquivo() { }
+    }
+}
+
